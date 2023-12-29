1a26a8b066468146dfc3068437f4d0d316df7d01
ManikantaMM <109547927+ManikantaMM@users.noreply.github.com>
2023-12-29 18:37:54 +0530

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -23,4 +23,9 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-java -jar target/${NAME}-${VERSION}.jar
+if [ -f "target/${NAME}-${VERSION}.jar" ]; then
+    # Running the Java application
+    java -jar target/${NAME}-${VERSION}.jar
+else
+    echo "Error: JAR file not found."
+fi
